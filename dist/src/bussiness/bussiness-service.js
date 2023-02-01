104425b89d4efb9d018fb91dcac673996f1ab2fa
antonlazukin <kevin6121991@gmail.com>
2023-02-01 14:21:07 +0400

    commit-15

--- dist/src/bussiness/bussiness-service.js
+++ dist/src/bussiness/bussiness-service.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.emailsManager = void 0;
 const email_adapter_1 = require("../adapters/email-adapter");
 const users_repository_db_1 = require("../repositories/users-repository-db");
+const uuid_1 = require("uuid");
 exports.emailsManager = {
     //
     sendEmailConfirmationMessage(email, code) {
@@ -29,18 +30,15 @@ exports.emailsManager = {
         return __awaiter(this, void 0, void 0, function* () {
             //find user
             const user = yield users_repository_db_1.usersRepository.findUserByLoginOrEmail(email);
-            if (user) {
+            if (user && (user === null || user === void 0 ? void 0 : user.emailConfirmation.isConfirmed) === false) {
                 //create new code if old one is not confirmed yet
-                if ((user === null || user === void 0 ? void 0 : user.emailConfirmation.isConfirmed) === false) {
-                    // user.emailConfirmation.confirmationCode = uuidv4()
-                    //create new code
-                    const newConfirmationCode = `<h1>Thank for your registration</h1>
+                user.emailConfirmation.confirmationCode = (0, uuid_1.v4)();
+                const newConfirmationCode = `<h1>Thank for your registration</h1>
         <p>To finish registration please follow the link below one more time:
             <a href='https://hw-7-gold.vercel.app/auth/confirm-email?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
         </p>`;
-                    //send email with new code
-                    return yield email_adapter_1.emailAdapter.sendEmail(email, newConfirmationCode, "confirm registration");
-                }
+                //send email with new code
+                return yield email_adapter_1.emailAdapter.sendEmail(email, newConfirmationCode, "confirm registration");
             }
             else {
                 return 400;
--- src/bussiness/bussiness-service.ts
+++ src/bussiness/bussiness-service.ts
@@ -20,18 +20,16 @@ export const emailsManager = {
     async sendEmailConfirmationMessageAgain(email: string) {
         //find user
         const user = await usersRepository.findUserByLoginOrEmail(email)
-        if (user) {
+        if (user && user?.emailConfirmation.isConfirmed === false) {
             //create new code if old one is not confirmed yet
-            if (user?.emailConfirmation.isConfirmed === false) {
-                // user.emailConfirmation.confirmationCode = uuidv4()
-                //create new code
-                const newConfirmationCode = `<h1>Thank for your registration</h1>
+            user.emailConfirmation.confirmationCode = uuidv4()
+            const newConfirmationCode = `<h1>Thank for your registration</h1>
         <p>To finish registration please follow the link below one more time:
             <a href='https://hw-7-gold.vercel.app/auth/confirm-email?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
         </p>`
-                //send email with new code
-                return await emailAdapter.sendEmail(email, newConfirmationCode, "confirm registration")
-            }
+            //send email with new code
+            return await emailAdapter.sendEmail(email, newConfirmationCode, "confirm registration")
+
         } else {
             return 400
         }
