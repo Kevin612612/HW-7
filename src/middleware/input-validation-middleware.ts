618fb0c6b4ec5c32bf2ec95f0ba843ef11d2fe77
antonlazukin <kevin6121991@gmail.com>
2023-02-01 12:33:50 +0400

    commit-16

--- src/middleware/input-validation-middleware.ts
+++ src/middleware/input-validation-middleware.ts
@@ -2,7 +2,7 @@
 
 
 
-import {body, param, header, CustomValidator} from 'express-validator'
+import {body, param, header} from 'express-validator'
 import {NextFunction, Request, Response} from "express";
 import {blogsRepository} from "../repositories/blogs-repository-db";
 import {postsRepository} from "../repositories/posts-repository-db";
@@ -154,12 +154,12 @@ export const usersEmailValidation1 = body('loginOrEmail')
 
 export const codeValidation = body('code')
     .custom(async value => {
-        const user = await usersCollection.findOne({'emailConfirmation.confirmationCode': value})
+        const user = await usersRepository.findUserByCode( value)
         if (!user) throw new Error('user with this code doesn\'t exist')
         return true
     })
     .custom(async value => {
-        const user = await usersCollection.findOne({'emailConfirmation.confirmationCode': value})
+        const user = await usersRepository.findUserByCode( value)
         if (user?.emailConfirmation.isConfirmed == true) throw new Error('Code already confirmed')
         return true
     })
