188ccfeeb2c6d93575e3256725d1d716aa4824c3
antonlazukin <kevin6121991@gmail.com>
2023-02-16 00:05:59 +0400

    commit-33

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -21,7 +21,7 @@ export const jwtService = {
             login: user.accountData.login,
             email: user.accountData.email
         }
-        const liveTime = 60
+        const liveTime = 10
         const accessToken = jwt.sign(
             payload,
             process.env.JWT_secret!,
@@ -41,7 +41,7 @@ export const jwtService = {
             login: user.accountData.login,
             email: user.accountData.email,
         }
-        const liveTime = 120
+        const liveTime = 20
         const refreshToken = jwt.sign(
             payload,
             process.env.JWT_secret!,
